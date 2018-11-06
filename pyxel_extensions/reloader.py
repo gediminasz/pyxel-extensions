f0af63d3d48daa12883c39370ccaa594c01c4f81
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2018-11-06 21:33:23 +0200

    Reload only stale modules

--- pyxel_extensions/reloader.py
+++ pyxel_extensions/reloader.py
@@ -1,11 +1,36 @@
+from dataclasses import dataclass
+from types import ModuleType
 import importlib
+import os
+
+
+@dataclass
+class HotModule:
+    module: ModuleType
+    last_reloaded_at: float = None
+
+    def __post_init__(self):
+        self.last_reloaded_at = self.file_modified_at
+
+    @property
+    def is_stale(self):
+        return self.file_modified_at > self.last_reloaded_at
+
+    def reload(self):
+        print(f'Reloading {self.module}')
+        importlib.reload(self.module)
+        self.last_reloaded_at = self.file_modified_at
+
+    @property
+    def file_modified_at(self):
+        return os.path.getmtime(self.module.__file__)
 
 
 class Reloader:
-    def __init__(self, hot_modules):
-        self.hot_modules = hot_modules
+    def __init__(self, modules):
+        self.hot_modules = tuple(HotModule(module=module) for module in modules)
 
     def reload(self):
-        print('Reloading...')
-        for module in self.hot_modules:
-            importlib.reload(module)
+        for hot_module in self.hot_modules:
+            if hot_module.is_stale:
+                hot_module.reload()
